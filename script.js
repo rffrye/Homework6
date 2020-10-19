45efa1833c73872c9654a0782f83e5c51282de4e
RFFRYE <rffrye@users.noreply.github.com>
2020-10-19 15:31:03 -0400

    worked on ajax request and noticied nothing is working in the console even though I have console logs.  Might need to look at my file links.

--- script.js
+++ script.js
@@ -24,4 +24,31 @@ $("#searchBtn").on("click", function() {
      // clear input box
     $("#searchTerm").val("");  
 
+     // full url to call api
+  const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  })
+  .then(function (response){
+
+    console.log(response)
+
+    console.log(response.name)
+    console.log(response.weather[0].icon)
+
+    let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    console.log(Math.floor(tempF))
+
+    console.log(response.main.humidity)
+
+    console.log(response.wind.speed)
+
+    getCurrentConditions(response);
+    getCurrentForecast(response);
+    makeList();
+
+    })
+
 });
