5eca7424ac66ffb1ddef1f566cebb35034a21272
RFFRYE <rffrye@users.noreply.github.com>
2020-10-19 13:26:30 -0400

    added enter key functionality to search button.  setting up search button to use input.

--- script.js
+++ script.js
@@ -3,3 +3,25 @@
 let city = $("#searchTerm").val();
 // store api key
 const apiKey = "&appid=957c40c40e959ff7f4bd70a494cfc79a";
+
+let date = new Date();
+
+$("#searchTerm").keypress(function(event) { 
+	
+	if (event.keyCode === 13) { //if user hits enter key.  Found on stackoverflow.
+		event.preventDefault();
+		$("#searchBtn").click(); 
+	} 
+});
+
+$("#searchBtn").on("click", function() {
+
+    $('#forecastH5').addClass('show');
+  
+    // get the value of the input from user
+    city = $("#searchTerm").val();
+
+     // clear input box
+    $("#searchTerm").val("");  
+
+});
