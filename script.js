dd220ff2cc3c0bbfb11bc49ca462fd79f5d1a27f
RFFRYE <rffrye@users.noreply.github.com>
2020-10-20 13:22:19 -0400

    forgot to commit often. Added the remaining functionality. May remove CSS and do inline for the one styling detail i used.

--- script.js
+++ script.js
@@ -75,11 +75,58 @@ function makeList() {
     var humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
     var wind = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
     var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
-
+   
     // add to page
     city.append(cityDate, image)
     cardBody.append(city, temperature, humidity, wind);
     card.append(cardBody);
     $("#currentCity").append(card)
    
+  }
+
+  function getCurrentForecast () {
+  
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey,
+      method: "GET"
+    }).then(function (response){
+  
+      console.log(response)
+      console.log(response.dt)
+      $('#forecast').empty();
+  
+      // variable to hold response.list
+      let results = response.list;
+      console.log(results)
+  
+
+      for (let i = 0; i < results.length; i++) {
+  
+        let day = Number(results[i].dt_txt.split('-')[2].split(' ')[0]);
+        let hour = results[i].dt_txt.split('-')[2].split(' ')[1];
+        console.log(day);
+        console.log(hour);
+  
+        if(results[i].dt_txt.indexOf("12:00:00") !== -1){
+          
+          // get the temperature and convert to fahrenheit 
+          let temp = (results[i].main.temp - 273.15) * 1.80 + 32;
+          let tempF = Math.floor(temp);
+  
+          const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+          const cardBody = $("<div>").addClass("card-body p-3 forecastBody")
+          const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+          const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
+          const humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
+  
+          const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results[i].weather[0].icon + ".png")
+  
+          cardBody.append(cityDate, image, temperature, humidity);
+          card.append(cardBody);
+          $("#forecast").append(card);
+  
+        }
+      }
+    });
+  
   }
